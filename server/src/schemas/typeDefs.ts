38c52ea01ab2d4ead82d7d3deb33e9fda288054d
Ian Ferguson <iansterlingferguson@gmail.com>
2024-10-17 14:31:04 -0700

    typeDefs.ts created for types User, Book, and Auth - UserInput and BookInput - Query and Mutations.

---
+++ server/src/schemas/typeDefs.ts
@@ -0,0 +1,52 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+    password: String
+  }
+
+  type Book {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input UserInput {
+    username: String!
+    email: String!
+    password: String!
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String
+    title: String!
+    bookId: String!
+    image: String
+    link: String
+  }
+  
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(input: UserInput!): Auth
+    saveBook(input: BookInput!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+export default typeDefs;
