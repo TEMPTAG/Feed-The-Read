5eb0e7d83ecc0986d194a51bd15fdab56608caf3
Ian Ferguson <iansterlingferguson@gmail.com>
2024-10-17 21:17:11 -0700

    updated server.ts to specify filename for render.

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,10 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { typeDefs, resolvers } from "./schemas/index.js";
 import { authenticateToken } from "./utils/auth.js";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Create a new Apollo Server instance with the provided typeDefs and resolvers
 const server = new ApolloServer({
