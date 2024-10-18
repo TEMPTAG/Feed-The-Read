71bd2ab05687d53734b55e5b653f22858983248f
Ian Ferguson <iansterlingferguson@gmail.com>
2024-10-17 22:14:14 -0700

    updated API info for vite.

--- client/src/utils/API.ts
+++ client/src/utils/API.ts
@@ -1,7 +1,7 @@
 // make a search to google books api
 // https://www.googleapis.com/books/v1/volumes?q=harry+potter
 export const searchGoogleBooks = (query: string) => {
-  const apiKey = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
+  const apiKey = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
   return fetch(
     `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`
   );
