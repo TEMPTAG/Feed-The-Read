b8b38534fb5138482c9505ce13326651d5fa0ac7
Ian Ferguson <iansterlingferguson@gmail.com>
2024-10-17 22:07:45 -0700

    added google books api key.

--- client/src/utils/API.ts
+++ client/src/utils/API.ts
@@ -1,5 +1,8 @@
 // make a search to google books api
 // https://www.googleapis.com/books/v1/volumes?q=harry+potter
 export const searchGoogleBooks = (query: string) => {
-  return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+  const apiKey = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
+  return fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`
+  );
 };
